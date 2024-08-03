59372ec38b221d63a93fe92cbc1c7aceb08240a4
joergbeyer <joerg.beyer@gmail.com>
2024-08-03 11:36:20 +0200

    initial commit

---
+++ build_deb.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+export DEBEMAIL="joerg.beyer@gmail.com"
+export DEBFULLNAME="Joerg Beyer"
+
+#mkdir minimonagent
+install -d minimonagent/usr/bin minimonagent/lib/systemd/system/ minimonagent/usr/share/doc/minimonagent minimonagent/DEBIAN
+
+cargo build --release --target=x86_64-unknown-linux-musl
+
+
+install DEBIAN/control minimonagent/DEBIAN/
+install DEBIAN/postinst minimonagent/DEBIAN/
+install -s target/x86_64-unknown-linux-musl/release/minimonagent minimonagent/usr/bin/minimonagent
+install -m 0644 minimonagent.service minimonagent/lib/systemd/system
+install -m 0644 changelog.Debian minimonagent/usr/share/doc/minimonagent/changelog.Debian
+gzip -n -f -9 minimonagent/usr/share/doc/minimonagent/changelog.Debian
+install -m 0644 copyright minimonagent/usr/share/doc/minimonagent/
+
+dpkg-deb --root-owner-group --build minimonagent
